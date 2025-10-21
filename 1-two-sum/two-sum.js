2175cf7d0433cbadcde609854a09ebbad4e7a594
Yeabsira(Dev) <155310056+yabulfc@users.noreply.github.com>
2025-10-21 15:14:28 +0300

    Time: 2 ms (70.80%) | Memory: 56.7 MB (34.42%) - LeetSync

---
+++ 1-two-sum/two-sum.js
@@ -0,0 +1,19 @@
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number[]}
+ */
+var twoSum = function(nums, target) {
+    let map = new Map();
+    for(let i =0 ; i< nums.length ; i++){
+        let num1 = nums[i];
+        let num2 = target - num1;
+        if(map.has(num2)) {
+            return [i,map.get(num2)];
+        }
+       map.set(num1,i);
+    }
+   
+    return    
+};
+
